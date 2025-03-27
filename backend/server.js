259947afb3d3fd1ef81e5c1ade4de7364e247207
kimternet <kimternet23@gmail.com>
2025-03-27 15:04:42 +0900

    백엔드와 프론트엔드 통합, cross env 설정

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 //const express = require("express");
-
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 
 import productRoutes from "./routes/product.route.js";
@@ -10,6 +10,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 // app.get("/products", (req, res) => {});
 
@@ -20,6 +22,14 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use("/api/products",productRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     connectDB();
